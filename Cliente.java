ca4b2517a6e7bbbe5841f26bddf76787ad883f5f
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2023-10-20 01:38:35 +0000

    Clases y atributos. Encapsulacion. GET y SET

--- Cliente.java
+++ Cliente.java
@@ -1,40 +1,92 @@
-
-
-
 import java.util.Date;
 
 public class Cliente {
+    // Estan privados porque esa es la ideologia de la encapsulacion de objetos en lenguajes orientados a objectos (OOP)
+  // Java es OOP por lo que nunca deberia accesarse los atributos directamente de otras clases si estan definido su set y get
+  // Esto puede dar pie a que otro cliente accese mi cuenta con codigo malicioso y otras practicas de codigo no deseadas
+    private int numeroCliente;
+    private String identificacion;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String telefono;
+    private String correoElectronico;
+    private Date fechaNacimiento;
 
-  public int numeroCliente;
-  public String identificacion;
-  public String nombre;
-  public String apellidos;
-  public String direccion;
-  public String telefono;
-  public String correoElectronico;
-  public Date fechaNacimiento;
+    public Cliente(int numeroCliente, String identificacion, String nombre, String apellidos, String direccion, String telefono, String correoElectronico, Date fechaNacimiento) {
+        this.numeroCliente = numeroCliente;
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.fechaNacimiento = fechaNacimiento;
+    }
 
-  public Cliente(
-    int numeroCliente,
-    String identificacion,
-    String nombre,
-    String apellidos,
-    String direccion,
-    String telefono,
-    String correoElectronico,
-    Date fechaNacimiento
-  ) {
-    this.numeroCliente = numeroCliente;
-    this.identificacion = identificacion;
-    this.nombre = nombre;
-    this.apellidos = apellidos;
-    this.direccion = direccion;
-    this.telefono = telefono;
-    this.correoElectronico = correoElectronico;
-    this.fechaNacimiento = fechaNacimiento;
-  }
+    public int getNumeroCliente() {
+        return numeroCliente;
+    }
 
-  public int getNumeroCliente() {
-    return numeroCliente;
-  }
+    public void setNumeroCliente(int numeroCliente) {
+        this.numeroCliente = numeroCliente;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    // You can add other methods and logic as needed.
 }
--- Transaccion.java
+++ Transaccion.java
@@ -1,60 +1,81 @@
-
-
-
 import java.util.Date;
 
 public class Transaccion {
-
-  public int numeroTransaccion;
-  public String tipo;
-  public int numeroCuenta;
-  public double saldoActual;
-  public double monto;
-  public Date fecha;
+    // Estan privados porque esa es la ideologia de la encapsulacion de objetos en lenguajes orientados a objectos (OOP)
+  // Java es OOP por lo que nunca deberia accesarse los atributos directamente de otras clases si estan definido su set y get
+  // Esto puede dar pie a que otro cliente accese mi cuenta con codigo malicioso y otras practicas de codigo no deseadas
+  private int numeroTransaccion;
+  private String tipo;
+  private int numeroCuenta;
+  private double saldoActual;
+  private double monto;
+  private Date fecha;
 
   public Transaccion(
+    int numeroTransaccion,
     String tipo,
     int numeroCuenta,
     double saldoActual,
-    double monto
+    double monto,
+    Date fecha
   ) {
-    int nextNumeroTransaccion = 0;
-    this.numeroTransaccion = nextNumeroTransaccion++;
+    this.numeroTransaccion = numeroTransaccion;
     this.tipo = tipo;
     this.numeroCuenta = numeroCuenta;
     this.saldoActual = saldoActual;
     this.monto = monto;
-    this.fecha = new Date();
+    this.fecha = fecha;
   }
 
+  /*
+  Se encapsularon todos los atributos con su get y set correspondiente
+  */
   public int getNumeroTransaccion() {
     return numeroTransaccion;
+  }
+
+  public void setNumeroTransaccion(int numeroTransaccion) {
+    this.numeroTransaccion = numeroTransaccion;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
   }
 
   public int getNumeroCuenta() {
     return numeroCuenta;
   }
 
-  /**
-   *
-   * @return
-   */
-  @Override
-  public String toString() {
-    return (
-      "Número de Transacción: " +
-      numeroTransaccion +
-      "\nTipo: " +
-      tipo +
-      "\nFecha: " +
-      fecha +
-      "\nNúmero de Cuenta: " +
-      numeroCuenta +
-      "\nSaldo Actual: " +
-      saldoActual +
-      "\nMonto: " +
-      monto +
-      "\n"
-    );
+  public void setNumeroCuenta(int numeroCuenta) {
+    this.numeroCuenta = numeroCuenta;
   }
+
+  public double getSaldoActual() {
+    return saldoActual;
+  }
+
+  public void setSaldoActual(double saldoActual) {
+    this.saldoActual = saldoActual;
+  }
+
+  public double getMonto() {
+    return monto;
+  }
+
+  public void setMonto(double monto) {
+    this.monto = monto;
+  }
+
+  public Date getFecha() {
+    return fecha;
+  }
+
+  public void setFecha(Date fecha) {
+    this.fecha = fecha;
+  }
+  // You can add other methods and logic as needed.
 }
